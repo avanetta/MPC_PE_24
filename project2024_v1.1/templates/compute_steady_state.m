e412cbaa7edca82b2b8ecbe66f684bb37a799b62
avanetta <avanetta@student.ethz.ch>
2024-04-26 11:21:45 +0200

    first try

--- project2024_v1.1/templates/compute_steady_state.m
+++ project2024_v1.1/templates/compute_steady_state.m
@@ -23,5 +23,8 @@ function [x_s, u_s] = compute_steady_state(params, d)
         % --> u_s = inv(B)*( (I-A)*x_s - Bd*d )
         I = eye(params.model.nx, params.model.nx);
         u_s = ((I-params.model.A)*x_s - params.model.Bd*d)\params.model.B; %gives error for singularity and dimensions connected to SS-computation
-
+        
+        %add comment to try git
+        kjbpronerpoicjporicaporcpeaorcn
+        
 end
