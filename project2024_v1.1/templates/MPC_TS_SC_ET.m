fc29c20e36e6f809290b7ba6c47ecd9191042a92
avanetta <avanetta@student.ethz.ch>
2024-05-14 17:06:02 +0200

    Ex 21 solved and tested

--- project2024_v1.1/templates/MPC_TS_SC_ET.m
+++ project2024_v1.1/templates/MPC_TS_SC_ET.m
@@ -21,42 +21,42 @@ classdef MPC_TS_SC_ET
             nx = params.model.nx;
             nu = params.model.nu;
 
-            nx_constr = size(S,1);
-            nt = size(S_t,1);
-
             H_u = params.constraints.InputMatrix;
             h_u = params.constraints.InputRHS;
             H_x = params.constraints.StateMatrix;
             h_x = params.constraints.StateRHS;
             
-            % Inifinite-horizon cost matrix P is also the solution to the ARE
-            [K,P,S] = dlqr(A, B, Q, R); %P is matrix for LQR infintite-horizon cost
+            nx_c = size(H_x,1);
+            nt = size(H,1);
             
-            % The optimization variables are U and X
+            % Inifinite-horizon cost matrix P is also the solution to the ARE
+            [K,P,S_LQR] = dlqr(A, B, Q, R); %P is matrix for LQR infintite-horizon cost
+            
+            % The optimization variables are U, X and E
             U = sdpvar(repmat(nu,1,N),repmat(1,1,N));
             X = sdpvar(repmat(nx,1,N+1),repmat(1,1,N+1));
-            E = sdpvar(repmat(nx_constr,1,N+1),repmat(1,1,N+1));
-            Et = sdpvar(repmat(nt,1,N+1),repmat(1,1,N+1));
+            E = sdpvar(repmat(nx_c,1,N+1), repmat(1,1,N+1));
+            E_t = sdpvar(nt,1,'full');
 
 
-            % Update constraints and obejctive function
+            % Update constraints and objective function
             constraints = [];
             objective = 0;
             X0 = X{1};
             for k = 1:N
                 objective = objective + X{k}'*Q*X{k} + U{k}'*R*U{k};
-                constraints = [constraints, X{k+1} == A*X{k} + B*U{k}];
-                constraints = [constraints, H_u*U{k} <= h_u, H_x*X{k}<=h_x];
-            end
-
-            for k = 1:N+1
                 objective = objective + E{k}'*S*E{k} + v*norm(E{k},1);
-                constraints = [constraints, H_u*U{k} <= h_u, H_x*X{k}<=h_x];
+                constraints = [constraints, X{k+1} == A*X{k} + B*U{k}];
+                constraints = [constraints, H_x*X{k}<=h_x+E{k}];
+                constraints = [constraints, H_u*U{k} <= h_u];
+                constraints = [constraints, E{k} >= 0];
             end
+            
+            objective = objective + E_t' * S_t * E_t + v_t*norm(E_t,1);
+            constraints = [constraints, H*X{N+1}<= h + E_t];
+            constraints = [constraints, E_t >= 0];
 
-            % FOR EX 21 ONLY
-            % Add constraint of x(N) and LQR IH cost
-            constraints = [constraints, H*X{N+1}<= h];
+            % Add IH LQR cost
             objective = objective + X{N+1}'*P*X{N+1};
             
             opts = sdpsettings('verbose', 1, 'solver', 'quadprog', 'quadprog.TolFun', 1e-8);
