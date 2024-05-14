888d084ed4c42bfd0c4cef9231a9557fa88c7a59
avanetta <avanetta@student.ethz.ch>
2024-05-14 13:31:01 +0200

    Ex 21 started

--- project2024_v1.1/templates/MPC_TS_SC_ET.m
+++ project2024_v1.1/templates/MPC_TS_SC_ET.m
@@ -15,6 +15,49 @@ classdef MPC_TS_SC_ET
         function obj = MPC_TS_SC_ET(Q, R, N, H, h, S, v, S_t, v_t, params)
             
             % ADD STUFF HERE
+            % Get numerical data defining system
+            A = params.model.A;
+            B = params.model.B;
+            nx = params.model.nx;
+            nu = params.model.nu;
+
+            nx_constr = size(S,1);
+            nt = size(S_t,1);
+
+            H_u = params.constraints.InputMatrix;
+            h_u = params.constraints.InputRHS;
+            H_x = params.constraints.StateMatrix;
+            h_x = params.constraints.StateRHS;
+            
+            % Inifinite-horizon cost matrix P is also the solution to the ARE
+            [K,P,S] = dlqr(A, B, Q, R); %P is matrix for LQR infintite-horizon cost
+            
+            % The optimization variables are U and X
+            U = sdpvar(repmat(nu,1,N),repmat(1,1,N));
+            X = sdpvar(repmat(nx,1,N+1),repmat(1,1,N+1));
+            E = sdpvar(repmat(nx_constr,1,N+1),repmat(1,1,N+1));
+            Et = sdpvar(repmat(nt,1,N+1),repmat(1,1,N+1));
+
+
+            % Update constraints and obejctive function
+            constraints = [];
+            objective = 0;
+            X0 = X{1};
+            for k = 1:N
+                objective = objective + X{k}'*Q*X{k} + U{k}'*R*U{k};
+                constraints = [constraints, X{k+1} == A*X{k} + B*U{k}];
+                constraints = [constraints, H_u*U{k} <= h_u, H_x*X{k}<=h_x];
+            end
+
+            for k = 1:N+1
+                objective = objective + E{k}'*S*E{k} + v*norm(E{k},1);
+                constraints = [constraints, H_u*U{k} <= h_u, H_x*X{k}<=h_x];
+            end
+
+            % FOR EX 21 ONLY
+            % Add constraint of x(N) and LQR IH cost
+            constraints = [constraints, H*X{N+1}<= h];
+            objective = objective + X{N+1}'*P*X{N+1};
             
             opts = sdpsettings('verbose', 1, 'solver', 'quadprog', 'quadprog.TolFun', 1e-8);
             obj.yalmip_optimizer = optimizer(constraints, objective, opts, X0, {U{1} objective});
