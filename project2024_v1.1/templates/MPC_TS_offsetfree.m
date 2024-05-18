971e72f8881d3545989cd92c0129cba607bf45a6
abersier <bersier.arnaud@gmail.com>
2024-05-19 00:56:52 +0200

    struggle with task 29

--- project2024_v1.1/templates/MPC_TS_offsetfree.m
+++ project2024_v1.1/templates/MPC_TS_offsetfree.m
@@ -17,7 +17,9 @@ classdef MPC_TS_offsetfree
             % ADD STUFF HERE
             A = params.model.A;
             B = params.model.B;
+            B_d = params.model.Bd;
             nx = params.model.nx;
+            nd = params.model.nd;
             nu = params.model.nu;
             
             H_u = params.constraints.InputMatrix;
@@ -25,14 +27,21 @@ classdef MPC_TS_offsetfree
             H_x = params.constraints.StateMatrix;
             h_x = params.constraints.StateRHS;
 
-            % The optimization variables are U and X
+            % Inifinite-horizon cost matrix P is also the solution to the ARE
+            [K,P,S] = dlqr(A, B, Q, R); %P is matrix for LQR infintite-horizon cost
+
+            % The optimization variables are U, X and D
             U = sdpvar(repmat(nu,1,N),repmat(1,1,N));
             X = sdpvar(repmat(nx,1,N+1),repmat(1,1,N+1));
+            D = sdpvar(repmat(nd,1,N+1),repmat(1,1,N+1));
+            XS = sdpvar(nx,1);
+            US = sdpvar(nu,1);
 
             % Update constraints and obejctive function
             constraints = [];
             objective = 0;
             X0 = X{1};
+            D0 = D{1};
 
             % "at each sampling time", means for each k (each step of the
             % horizon)
@@ -40,15 +49,27 @@ classdef MPC_TS_offsetfree
                 % Estimate state and disturbance x_est, d_est
 
                 % Obtain (xs , us) from steady-state target problem using disturbance estimate
-                [x_s,u_s] = compute_steady_state(params,D_est(:,k));
+                % [x_s,u_s] = compute_steady_state(params,D{k});
+                % it seems like params.model has no C_ref matrix as
+                % element, which would be used inside of
+                % compute_steady_state
+                
 
-                objective = objective + X{k}'*Q*X{k} + U{k}'*R*U{k};
-                constraints = [constraints, X{k+1} == A*X{k} + B*U{k}];
+                objective = objective + (X{k}-x_s)'*Q*(X{k}-x_s) + (U{k}-u_s)'*R*(U{k}-u_s);
+                constraints = [constraints, X{k+1} == A*X{k} + B*U{k} + B_d*D{k}];
+                constraints = [constraints, D{k+1} == D{k}];
                 constraints = [constraints, H_u*U{k} <= h_u, H_x*X{k}<=h_x];
+
             end
 
+            % Add constraint of x(N) and LQR IH cost
+            constraints = [constraints, H*(X{N+1}-x_s)<= h];
+            objective = objective + (X{N+1}-x_s)'*P*(X{N+1}-x_s);
+
+            % This was given
             opts = sdpsettings('verbose',1,'solver','quadprog');
-            obj.yalmip_optimizer = optimizer(constraints,objective,opts,{X0 XS US},{U{1} objective});
+            obj.yalmip_optimizer = optimizer(constraints,objective,opts,{X0 D0 XS US},{U{1} objective});
+        
         end
 
         function [u, ctrl_info] = eval(obj,x,d,x_s,u_s)
@@ -56,7 +77,7 @@ classdef MPC_TS_offsetfree
             % ctrl_info = .;
             %% evaluate control action by solving MPC problem, e.g.
             tic
-            [optimizer_out,errorcode] = obj.yalmip_optimizer(x);
+            [optimizer_out,errorcode] = obj.yalmip_optimizer(x, d, x_s, u_s);
             solvetime = toc;
             [u, objective] = optimizer_out{:};
 
